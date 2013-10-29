fa4cf30da2e7231b689ebbbb5b37ab84b6d5e1ab
Adam Miribyan <adam.miribyan@gmail.com>
2013-10-29 02:29:28 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dandelyse::Application.config.secret_key_base = 'd253dd9ab208baa277860392cbd4af48edadbbaebb340fe8e1e3ed76cec83912dd3732a10e1e08ae5b5a4c2773cdab03c0c6d5bef564a1cba14f86edcf704226'
